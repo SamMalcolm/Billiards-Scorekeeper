04ba1bc63e2843aa2056617746c23cf45fe3fad2
SamMalcolm <sam_malcolm@live.com.au>
2024-02-20 09:45:34 +1100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'English Billiards',
       theme:
           ThemeData(primarySwatch: Colors.green, brightness: Brightness.dark),
